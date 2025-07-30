177cfbfc96bf982355d8423926b0a1998bef5269
flatlayn <raistephen.isidro@gmail.com>
2025-07-30 18:26:48 +0800

    Complete function and printing, input required.

--- LBYARCH/asmfunc.asm
+++ LBYARCH/asmfunc.asm
@@ -1,18 +1,37 @@
 ; assembly part using x86-64
 
 section .data
-msg db "Hello World", 13,10,0
+; RCX - width, RDX - height, R8 - float*, R9 - int*
+scale dd: 255.0
 
 section .text
 bits 64
 default rel
 
-global asmhello
-extern printf
+global imgCvtGrayFloatToInt
 
-asmhello:
-	SUB RSP, 8*5	; caller
-	LEA rcx, [msg] 
-	call printf
-	add RSP, 8*5
+imgCvtGrayFloatToInt:
+	
+	MOV RAX, RCX	; First parameter
+	IMUL RAX, RDX	; First Parameter * Second Parameter = Size
+	MOV R10, RAX	; Store size at R10 for counter limit
+	XOR R11, R11	; Loop counter reset
+
+	MOVSS XMM1, dword [scale]		; Scale
+
+.LOOP:
+	CMP R11, R10
+	JGE .END
+    MOVSS XMM0, [R8 + R11*4]
+    MULSS XMM0, XMM1
+	CVTSS2SI EAX, XMM0
+	MOV [R9 + R11*4], EAX
+	INC R11
+	JMP .LOOP
+
+.END:
+	leave
 	ret
+
+
+	
--- LBYARCH/main.c
+++ LBYARCH/main.c
@@ -2,9 +2,50 @@
 #include <stdlib.h>
 #include <Windows.h>
 
-extern void asmhello();
+//extern void asmhello();
+extern void imgCvtGrayFloatToInt(int width, int height, float* img, int* conv);
 
 int main() {
-	asmhello();
+	int width, height;
+	
+	printf("Enter width and height: ");
+	scanf("%d %d", &width, &height);
+
+	while (width <= 0 || height <= 0) {
+		printf("Invalid inputs, please enter width and height: ");
+		scanf("%d %d", &width, &height);
+	}
+
+	float* img = malloc(sizeof(float) * (width * height));
+	int* converted = malloc(sizeof(int) * (width * height));
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			printf("Pixel values (Row %d): ", i+1);
+			scanf("%f ",);
+		}
+	}
+
+	/*
+	* while (input >= 1.0 || input <= 0.0) {
+	*	invalid;
+	*/
+
+	imgCvtGrayFloatToInt(width, height, img, converted);
+	int k = 0;
+
+	do {
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				printf("%d ", converted[k]);
+				k++;	
+			}
+			printf("\n");
+			k++;
+		}
+	} while (k < (width * height));
+
+	
 	return 0;
 }
+
